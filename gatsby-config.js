19b557fb77ed15424b7fff1f3fd4174f838c315e
Kamil Unai <kamilxunai@yahoo.com>
2020-02-16 21:48:02 +0100

    add posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,11 +7,19 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/src/content`,
       },
     },
     `gatsby-transformer-sharp`,
