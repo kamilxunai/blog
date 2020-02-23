4d79a26a1ea511aa0bdee624c53f3a70e9525b6a
Kamil Unai <kamilxunai@yahoo.com>
2020-02-23 22:33:06 +0100

    add blog listing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,37 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data: { pages } }) => (
   <Layout>
     <SEO title="home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div>
+      {pages.nodes.map(({ frontmatter }) => (
+        <Link key={frontmatter.path} to={frontmatter.path}>
+          {frontmatter.title}
+        </Link>
+      ))}
     </div>
     <Link to="/about/">Go to page 2</Link>
   </Layout>
 )
+
+export const query = graphql`
+  query HomePageQuery {
+    pages: allMdx {
+      nodes {
+        frontmatter {
+          path
+          title
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
