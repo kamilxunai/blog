be0d270d7a597d7618360d3ba21256cbe5440654
Kamil Unai <kamilxunai@yahoo.com>
2020-04-30 20:08:15 +0200

    rewrite basic style to tailwind

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -24,20 +24,9 @@ const Layout: FC = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
+      <div className="max-w-4xl pt-0 px-4 pb-6 my-0 mx-auto">
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        <footer>© {new Date().getFullYear()}</footer>
       </div>
     </>
   )
