f62308237e39188786e9b2ded0956a57adfe5542
Kamil Unai <kamilxunai@yahoo.com>
2020-09-05 14:16:42 +0200

    update eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,6 +29,7 @@ module.exports = {
     "react/prop-types": "off", // Disable prop-types as we use TypeScript for type checking
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/camelcase": "off",
+    "@typescript-eslint/ban-ts-comment": "off"
   },
   overrides: [
     // Override some TypeScript rules just for .js files
