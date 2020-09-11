730d6c4c28405524bf7baa0bb7c54c83641b9606
Kamil Unai <kamilxunai@yahoo.com>
2020-09-11 20:43:50 +0200

    make lint happy

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,14 +29,14 @@ module.exports = {
     "react/prop-types": "off", // Disable prop-types as we use TypeScript for type checking
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/camelcase": "off",
-    "@typescript-eslint/ban-ts-comment": "off"
+    "@typescript-eslint/ban-ts-comment": "off",
   },
   overrides: [
     // Override some TypeScript rules just for .js files
     {
       files: ["*.js"],
       rules: {
-        "@typescript-eslint/no-var-requires": "off", //
+        "@typescript-eslint/no-var-requires": "off",
       },
     },
   ],
